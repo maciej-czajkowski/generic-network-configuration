5022264202873fb2b4572f2d2efc95694df725a8
Maciej Czajkowski <maciejpiotrczajkowski@gmail.com>
2021-03-19 15:52:34 +0100

    add translator and dictionaries for cisco - generic

--- test/main.py
+++ test/main.py
@@ -1,7 +1,18 @@
 from netconfparsers import CiscoConfigParser
+from translators import *
 from anytree import RenderTree, AsciiStyle
-
+from anytree.exporter import JsonExporter
 
 config = CiscoConfigParser("ciscoexample.txt")
 root = config.parse()
+
 print(RenderTree(root, style=AsciiStyle()).by_attr())
+exporter = JsonExporter(indent=4)
+
+
+trans = Cisco2GenericTranslator(root)
+trans.translate()
+print(trans.get_json(4))
+
+file = open("output.json", "w")
+file.write(trans.get_json(4))
