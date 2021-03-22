4a70fe074ba10ed0caef6e219ae27444eb0d59b8
Maciej Czajkowski <maciejpiotrczajkowski@gmail.com>
2021-03-22 21:15:52 +0100

    Add cisco generator
    commit Łukasza P., ja tylko poprawilem

--- test/main.py
+++ test/main.py
@@ -1,15 +1,23 @@
 from netconfparsers import JuniperConfigParser, CiscoConfigParser
 from translators import *
-
+from ConfigGenerator import ciscoGenerator
 from anytree import RenderTree, AsciiStyle
 from anytree.exporter import JsonExporter
 
 config = JuniperConfigParser("juniperexample.txt")
+configC = CiscoConfigParser("ciscoexample.txt")
 
-
+root2 = configC.parse()
 root = config.parse()
 
-print(RenderTree(root, style=AsciiStyle()).by_attr())
+print(RenderTree(root2, style=AsciiStyle()).by_attr())
+
+generator = ciscoGenerator(root2)
+cfg = generator.cfgGenerator()
+generator.write_to_file("generatedCisco.txt", cfg)
+# for i in generator.cfgGenerator():
+#     print(i)
+
 exporter = JsonExporter(indent=4)
 
 
