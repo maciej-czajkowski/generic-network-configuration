8509fc5db8f7f3e8b3cd74f81eec3dc66099aa9d
Maciej Czajkowski <maciejpiotrczajkowski@gmail.com>
2021-05-05 21:58:16 +0200

    Create config.py to parse to tree to json

--- project/gennetconf.py
+++ project/gennetconf.py
@@ -1,5 +1,7 @@
 import sys
-sys.path.append('./gennetconf/')
+sys.path.append('gennetconf/')
 from generators import *
 from parsers import *
 from translators import *
+from config import *
+from input_translator import *
--- project/main.py
+++ project/main.py
@@ -1,18 +1,49 @@
 import gennetconf as cfg
+from anytree import RenderTree
+import json as js
 
-# cisco -> cisco tree
+def printHeader(string):
+    print("###"*10)
+    print("### ", string, " ###")
+    print("###"*10)
+
+
+
 cisco_config_tree = cfg.CiscoConfigParser("input/ciscoexample.txt").parse()
-# print(RenderTree(cisco_config_tree, style=AsciiStyle()).by_attr())
 
-# cisco_tree -> generic
-generic_cfg = cfg.Cisco2GenericTranslator(cisco_config_tree).get_json(4)
-# print(generic_cfg)
+printHeader("Input tree")
+print(RenderTree(cisco_config_tree).by_attr('name'))
 
-# generic -> juniper_tree
-juniper_config_tree = cfg.Generic2JuniperTranslator(generic_cfg).get_root()
-# print(RenderTree(juniper_config_tree, style=AsciiStyle()).by_attr())
+printHeader("TREE => JSON")
 
-# juniper_tree -> juniper
-juniper_config = cfg.JuniperConfigGenerator(juniper_config_tree)
-juniper_config.write_to_file("gen/generatedJuniper.cfg")
+example_json = cfg.ConfigJson(tree=cisco_config_tree)
+example_json.parse_tree()
+json = example_json.get_json(4)
+print(json)
+
+printHeader("TREE => TREE")
+
+
+example_tree = cfg.ConfigTree(tree=cisco_config_tree)
+example_tree.parse_tree()
+tree = example_tree.get_tree()
+print(RenderTree(tree).by_attr('name'))
+
+
+
+
+file = open("input/sampleJson.json", "r").read()
+json_in = js.loads(file)
+
+printHeader("Input Json")
+print(file)
+
+printHeader("JSON => TREE")
+
+example2 = cfg.ExampleJsonToAll(json=json_in)
+example2.parse_json()
+output_tree = example2.get_tree()
+print(RenderTree(output_tree).by_attr('name'))
+
+
 
