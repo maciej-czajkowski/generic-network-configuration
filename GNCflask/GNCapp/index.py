63c4780b6d2809d942a98af419c75f1f50cae487
Paweł Jędrzejczyk <jedrzejczyk.pawel@outlook.com>
2021-06-07 00:33:12 +0200

    add restriction in accesing /index for not logged users

--- GNCflask/GNCapp/index.py
+++ GNCflask/GNCapp/index.py
@@ -20,75 +20,81 @@ app.config['UPLOAD_FOLDER'] = "GNCapp/static/uploads/"  #
 # just redirect to index
 @bp.route('/')
 def redirect_to_index():
-    return redirect('/index')
+    if 'user_id' not in session:
+        return redirect('/auth/login')
+    else:
+        return redirect('/index')
 
 
 @bp.route('/index', methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        if request.files:
-            file = request.files["file"]
-            print(file)
-            db = get_db()
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-            db.execute(
-                'INSERT INTO user_files (owner_id, file_full_name) VALUES (?, ?)',
-                (str(session['user_id']), file.filename)
-            )
-            db.commit()
-            print('file saved')
-            return redirect(request.url)
-        # add usage of db
-    if request.method == "GET":
-        print("GETT")
-        req = request.args
-        print(req)
-        file_name = request.args.get('filename')
-        if request.args.get('filename'):
-            outputFilename = ""
-            error = None
-            if request.args.get('input') == 'inputCisco':
-                if request.args.get('output') == 'outputJSON':
-                    ciscoToJSON(file_name)
-                    outputFilename = file_name + ".json"
-                elif request.args.get('output') == 'outputJuniper':
-                    ciscoToJuniper(file_name)
-                    outputFilename = file_name + ".txt"
+    if 'user_id' not in session:
+        return redirect('/auth/login')
+    else:
+        if request.method == "POST":
+            if request.files:
+                file = request.files["file"]
+                print(file)
+                db = get_db()
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+                db.execute(
+                    'INSERT INTO user_files (owner_id, file_full_name) VALUES (?, ?)',
+                    (str(session['user_id']), file.filename)
+                )
+                db.commit()
+                print('file saved')
+                return redirect(request.url)
+            # add usage of db
+        if request.method == "GET":
+            print("GETT")
+            req = request.args
+            print(req)
+            file_name = request.args.get('filename')
+            if request.args.get('filename'):
+                outputFilename = ""
+                error = None
+                if request.args.get('input') == 'inputCisco':
+                    if request.args.get('output') == 'outputJSON':
+                        ciscoToJSON(file_name)
+                        outputFilename = file_name + ".json"
+                    elif request.args.get('output') == 'outputJuniper':
+                        ciscoToJuniper(file_name)
+                        outputFilename = file_name + ".txt"
+                    else:
+                        error = 'There is no point in translating Cisco to Cisco'
+                        print('cisco to cisco translating')
+                elif request.args.get('input') == 'inputJuniper':
+                    if request.args.get('output') == 'outputCisco':
+                        juniperToCisco(file_name)
+                        outputFilename = file_name + ".txt"
+                    elif request.args.get('output') == 'outputJSON':
+                        juniperToJSON(file_name)
+                        outputFilename = file_name + ".json"
+                    else:
+                        error = 'There is no point in translating Juniper to Juniper'
+                        print('juniper to juniper translating')
+                elif request.args.get('input') == 'inputJSON':
+                    if request.args.get('output') == 'outputCisco':
+                        JSONToCisco(file_name)
+                        outputFilename = file_name + ".txt"
+                    elif request.args.get('output') == 'outputJuniper':
+                        JSONToJuniper(file_name)
+                        outputFilename = file_name + ".txt"
+                    else:
+                        error = 'There is no point in translating JSON to JSON'
+                        print('json to json translating')
+                if error is None:
+                    try:
+                        return send_from_directory(app.config["UPLOAD_FOLDER"], filename=outputFilename, as_attachment=True)
+                    except FileNotFoundError:
+                        print("File not found")
+                        abort(404)
                 else:
-                    error = 'There is no point in translating Cisco to Cisco'
-                    print('cisco to cisco translating')
-            elif request.args.get('input') == 'inputJuniper':
-                if request.args.get('output') == 'outputCisco':
-                    juniperToCisco(file_name)
-                    outputFilename = file_name + ".txt"
-                elif request.args.get('output') == 'outputJSON':
-                    juniperToJSON(file_name)
-                    outputFilename = file_name + ".json"
-                else:
-                    error = 'There is no point in translating Juniper to Juniper'
-                    print('juniper to juniper translating')
-            elif request.args.get('input') == 'inputJSON':
-                if request.args.get('output') == 'outputCisco':
-                    JSONToCisco(file_name)
-                    outputFilename = file_name + ".txt"
-                elif request.args.get('output') == 'outputJuniper':
-                    JSONToJuniper(file_name)
-                    outputFilename = file_name + ".txt"
-                else:
-                    error = 'There is no point in translating JSON to JSON'
-                    print('json to json translating')
-            if error is None:
-                try:
-                    return send_from_directory(app.config["UPLOAD_FOLDER"], filename=outputFilename, as_attachment=True)
-                except FileNotFoundError:
-                    print("File not found")
-                    abort(404)
-            else:
-                flash(error)
+                    flash(error)
 
-        else:
-            print("empty")
-    return render_template('index.html')
+            else:
+                print("empty")
+        return render_template('index.html')
 
 
 # download with direct link
